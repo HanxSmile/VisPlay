98d13a2ba0638379b0e5ac5779af2a73483dd954
韩笑 <hanxiao36@midea.com>
2025-12-25 18:07:55 +0800

    None

--- vllm_service_init/make_one_sample.py
+++ vllm_service_init/make_one_sample.py
@@ -1,3 +1,28 @@
+from io import BytesIO
+from PIL import Image
+import base64
+import json
+
+
 dic = {
-    "image"
+    "image": "/data/hanxiao36/2383.202541811132.2.156.14702.30.1000.3.3202504180618585904.20250418061858.110132363711.0788.jpg",
+    "question": "请为下面问题选择一个或多个正确答案，多个答案以\",\"分割。在回答之前，请仔细观察并思考病症可能发生的区域和表现。问题：根据胸部X光报告，以下哪些影像学表现支持慢性支气管炎伴肺气肿的诊断？备选答案：A: 气胸, B: 胸腔积液, C: 肺气肿, D: 肺不张。请按照<think>...</think><answer>...</answer>格式进行输出。",
+    "type": "multi_choice",
+    "system": "你是一名资深放射科医生，正在阅读一个病人的胸部X光片（可能包括正位片和/或侧位片）。",
 }
+
+def image_to_base64(img):
+    output_buffer = BytesIO()
+    img.save(output_buffer, format='png')
+    byte_data = output_buffer.getvalue()
+    base64_str = base64.b64encode(byte_data)
+    return "data:image/png;base64," + base64_str.decode()
+
+
+image = dic["image"]
+image = Image.open(image).convert("RGB")
+
+dic["image"] = image_to_base64(image)
+
+with open("demo.json", "w", encoding="utf-8") as f:
+    json.dump([dic] * 10, f, indent=4)
